828c913fb5d7f5d73825159fc66847c4c239d692
mahdi <mahdimamashli1383@gmail.com>
2024-05-01 11:44:19 +0330

    "Simplified is\_palindrome function and implemented an interactive palindrome game with scoring system"
    The diff includes the following changes:
    
    * The `is_palindrome` function was simplified to check for pal

--- Interactive_Palindrome_Game/Interactive-Palindrome-Game .py
+++ Interactive_Palindrome_Game/Interactive-Palindrome-Game .py
@@ -6,59 +6,31 @@ def generate_string(length):
   return random_string
 
 def is_palindrome(text):
-        processed_text = "#" + "#".join(text) + "#"
-        C = [0] * (2 * len(processed_text) + 1)  
-        P = [0] * (2 * len(processed_text) + 1) 
-        C_center = 0
-        R = 0
-        for i in range(1, len(processed_text) - 1):
-            i_mirror = 2 * C_center - i
-            if i <= R:
-                P[i] = min(R - i, P[i_mirror])  
-            else:
-                P[i] = 0
-            
-            while (i - P[i] - 1 >= 0 and i + P[i] + 1 < len(processed_text) and
-                   processed_text[i - P[i] - 1] == processed_text[i + P[i] + 1]):
-                P[i] += 1
+  processed_text = text.lower()  # Convert to lowercase for case-insensitive check
+  # Iterate through middle characters only
+  for i in range(len(processed_text) // 2):
+      # Check if characters on either side of the center match
+      if processed_text[i] != processed_text[-(i + 1)]:
+          return False
+  return True
 
-            if i + P[i] > R:
-                C_center = i
-                R = i + P[i]
+score = 0
 
-        palindromes = []
-        for i in range(1, len(processed_text) - 1, 2):
-            if P[i] > 0:
-                start_index = (i - P[i]) // 2
-                length = P[i]
-                palindromes.append((start_index, length))
+while True:
+  random_string = generate_string(random.randint(5, 20))
+  print("Is the string a palindrome?")
+  print(random_string)
+  user_guess = input("Enter your guess (palindrome/not palindrome): ").lower()
+  is_correct = is_palindrome(random_string) == (user_guess == "palindrome")
+  if is_correct:
+    print("Correct!")
+    score+=1
+  else:
+    print("Incorrect.")
+  print("Your current score:", score)
+  print("Play another round? (y/n)")
+  play_again = input().lower()
+  if play_again != "y":
+    break
 
-        return palindromes
-
-random_string = generate_string(random.randint(5, 20))
-print(random_string)
-debug = is_palindrome(random_string)
-print("is palindrome?:", debug)
-
-# score = 0
-
-# while True:
-#   random_string = generate_string(random.randint(5, 20))
-#   print("Is the string a palindrome?")
-#   print(random_string)
-#   debug = is_palindrome(random_string)
-#   print("debug:",debug)
-#   user_guess = input("Enter your guess (palindrome/not palindrome): ").lower()
-#   is_correct = is_palindrome(random_string) == (user_guess == "palindrome")
-#   if is_correct:
-#     print("Correct!")
-#     score+=1
-#   else:
-#     print("Incorrect.")
-#   print("Your current score:", score)
-#   print("Play another round? (y/n)")
-#   play_again = input().lower()
-#   if play_again != "y":
-#     break
-
-# print("Thanks for playing!")
+print("Thanks for playing!")
