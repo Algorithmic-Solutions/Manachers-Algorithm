6efd235c2aaad2971116992bd131690194921c4c
mahdi <mahdimamashli1383@gmail.com>
2024-05-01 16:22:09 +0330

    "Update PalindromeFinder class: change word joining, modify Manacher's algorithm to return palindrome arrays"
    This commit updates the PalindromeFinder class by modifying the word joining in the cleanText method from using a

--- Palindrome_Finder/Palindrome-Finder.ts
+++ Palindrome_Finder/Palindrome-Finder.ts
@@ -10,22 +10,28 @@ class PalindromeFinder {
     let words: string[] = this.text
       .split(/\s+/)
       .filter((word) => !stopwords.includes(word));
-    return words.join(" ");
+    return words.join("");
   }
-  manachers(): number[] {
-    const processed_text = `#${this.text.split("").join("#")}#`;
+  manachers(): number[][] {
+    console.log("this.word:",this.word);
+    const processed_text = `#${this.word.split("").join("#")}#`;
     let n = processed_text.length;
-    let P = new Array(2 * n).fill(0);
+    let P = new Array(2*n+1).fill(0);
     let center = 0,
       r = 0;
-    for (let i = 0; i < n - 1; i++) {
+    for (let i = 1; i < n-1; i++) {
       let i_mirror = 2 * center - i;
-      if (i < r) {
+      if (i <= r) {
         P[i] = Math.min(r - i, P[i_mirror]);
       } else {
         P[i] = 0;
       }
-      while (processed_text[i - P[i] - 1] === processed_text[i + P[i] + 1]) {
+      while (
+        i - P[i] - 1 >= 0 &&
+        i + P[i] + 1 < processed_text.length &&
+        processed_text[i - P[i] - 1] ===
+          processed_text[i + P[i] + 1]
+      ) {
         P[i]++;
       }
       if (i + P[i] > r) {
@@ -33,23 +39,19 @@ class PalindromeFinder {
         r = i + P[i];
       }
     }
-    return P;
+    const palindromes: number[][] = [];
+    for (let i = 1; i < processed_text.length - 1; i += 2) {
+      if (P[i] > 0) {
+        const startIndex = (i - P[i]) / 2;
+        const length = P[i];
+        palindromes.push([startIndex, length]);
+      }
+    }
+    return palindromes;
   }
-  find_palindromes():void {
+  find_palindromes(): void {
     const p = this.manachers();
     console.log("p:", p);
-    // console.log("Found Palindromes:")
-    // console.log("| Start Index | Length | Palindrome |")
-    // console.log("|-------------|--------|------------|")
-    // for (let i = 0; i < p.length-1; i++) {
-    //     if (p[i]>0) {
-    //         const startIdx = Math.floor((i - p[i]) / 2);
-    //         const length = p[i];
-    //         let palindrome = this.word.slice(startIdx,startIdx + length)
-    //         console.log(`| ${startIdx}           | ${length}      | ${palindrome} |`)
-
-    //     }        
-    // }
   }
 }
 const text = "The quick brown fox jumps over the lazy dog.";
